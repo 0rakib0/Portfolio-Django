fc33697024b94ba0a824087e6e1cc29c06b26834
rakib <hassanrakibul926@gmail.com>
2023-12-29 20:57:40 +0600

    hello

--- personalPortfolio/wsgi.py
+++ personalPortfolio/wsgi.py
@@ -1,11 +1,3 @@
-"""
-WSGI config for personalPortfolio project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
 
 import os
 
@@ -14,4 +6,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personalPortfolio.settings')
 
 application = get_wsgi_application()
-
+app = application
