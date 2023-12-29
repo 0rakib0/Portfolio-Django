9b4d1cffe4d30578860ac9a1ff6b20e605f21427
rakib <hassanrakibul926@gmail.com>
2023-12-29 19:05:21 +0600

    deploy configaration

--- personalPortfolio/settings.py
+++ personalPortfolio/settings.py
@@ -26,9 +26,9 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = 'django-insecure-pb5wlywo)w&-$62fjl^gb0mu48@d_wb81=gjah9zr2e=_lic)!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -77,12 +77,24 @@ WSGI_APPLICATION = 'personalPortfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '4g13C*F-B-1-FD5dfbDae-A6Ef*g6-c*',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '13489',
     }
 }
+
 
 
 # Password validation
@@ -122,13 +134,21 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [STATIC_DIR]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATICFILES_DIRS = [STATIC_DIR]
 
 # media file setting
 MEDIA_ROOT  = MEDIA_DIR
 MEDIA_URL = '/media/'
 
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
