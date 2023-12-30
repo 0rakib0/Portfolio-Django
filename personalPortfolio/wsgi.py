14e1ebc704c6a83b3b50ad108a82259e347f9e61
rakib <hassanrakibul926@gmail.com>
2023-12-30 13:29:36 +0600

    replce wasgy file

--- personalPortfolio/wsgi.py
+++ personalPortfolio/wsgi.py
@@ -7,23 +7,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personalPortfolio.settings')
 
 application = get_wsgi_application()
 app = application
-
-personalPortfolio/wsgi.py
-
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-# from whitenoise import WhiteNoise
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personalPortfolio.settings')
-
-# # Get the Django application
-# django_application = get_wsgi_application()
-
-# # Wrap the Django application with WhiteNoise
-# application = WhiteNoise(django_application)
-
-# # Add the static and media file serving configurations
-# application.add_files(os.path.join(os.path.dirname(__file__), 'staticfiles_build'), prefix='/static/')
-# application.add_files(os.path.join(os.path.dirname(__file__), 'media'), prefix='/media/')
-
